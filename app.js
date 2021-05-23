dc45a8568df20b8abbf2515f71dc3cc65a099e43
abdias mb <cubecompany.info@gmail.com>
2021-05-23 18:19:14 -0500

    multiplicacion en consola

---
+++ app.js
@@ -0,0 +1,11 @@
+const { crearArchivo } = require('./helpers/multiplicar');
+
+const argv = require('./config/yargs');
+
+console.clear();
+
+console.log(argv)
+
+crearArchivo(argv.base, argv.listar, argv.hasta)
+    .then(nombreArchivo => { console.log(nombreArchivo)})
+    .catch(console.log);
---
+++ salida/info.md
@@ -0,0 +1,9 @@
+# informacion basica
+Esta carperta es para alamacenar todos los doc de la tabla de multiplicacion
+
+# base
+--base o --b  => numero a multiplicar
+# listar
+--listar o --l => para mostrar o listar en consola
+# hasta
+--hasta o --h => hasta que numero multiplicar
